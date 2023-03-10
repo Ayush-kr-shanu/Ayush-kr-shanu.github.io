a7b61e8918d718d0f761b269c40ec1bab99da5d8
Ayush Kr Shanu <112754570+Ayush-kr-shanu@users.noreply.github.com>
2023-03-10 13:45:30 +0530

    Ayush

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend developer", "Backend developer", "Full Stack-Web developer"],
+    strings: ["Backend developer", "Full Stack-Web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
