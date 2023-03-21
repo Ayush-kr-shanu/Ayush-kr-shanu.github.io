3c2e08972bb372d4cc26449fbc1b64b4c53a516f
Ayush Kr Shanu <112754570+Ayush-kr-shanu@users.noreply.github.com>
2023-03-21 11:43:32 +0530

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -219,9 +219,6 @@ srtop.reveal('.home .content .btn', { delay: 200 });
 srtop.reveal('.home .image', { delay: 400 });
 srtop.reveal('.home .linkedin', { interval: 600 });
 srtop.reveal('.home .github', { interval: 800 });
-srtop.reveal('.home .twitter', { interval: 1000 });
-srtop.reveal('.home .telegram', { interval: 600 });
-srtop.reveal('.home .instagram', { interval: 600 });
 srtop.reveal('.home .dev', { interval: 600 });
 
 /* SCROLL ABOUT */
